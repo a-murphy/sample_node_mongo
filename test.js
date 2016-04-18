0ec5ae8d5d06fdbdf268d6c793d34fa40267c4e4
a-murphy <a-murphy@users.noreply.github.com>
2016-04-18 09:38:13 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,37 +1,19 @@
 var superagent = require("superagent"),
-    chai = require("chai"),
-    expect = chai.expect,
-    should = require("should");
+    moucha = require("mocha");
 
-describe("Index", function () {
-  it("renders HTML", function (done) {
-    superagent.get("http://localhost:3000/")
-      .end(function (e, res) {
-        (e === null).should.equal(true);
-        res.text.should.equal("Hey buddy!");
-        done();
-      });
-  });
-});
+describe("test", function () {
+  it.only('should default the new user to active, but unconfirmed', function (done) {
+    User.forge()
+        .save({ email: 'test@test.com', password: 'password' })
+        .then(function (savedUser) {
+            return savedUser.fetch();
+        })
+        .then(function (user) {
+            console.log(user);
+            user.get('email').should.eql('test@test.com');
+            Number(user.get('active')).should.eql(1);
+            Number(user.get('confirmed')).should.eql(0);
 
-describe("Persistence", function () {
-  it("should create a thing", function (done) {
-    superagent.get("http://localhost:3000/doobie")
-      .end(function (e, res) {
-        (e === null).should.equal(true);
-      //  var response = (res.indexOf("new") !== -1);
-        expect(res);
-        done();
-      });
-  });
-  it("should retrieve a thing", function (done) {
-    superagent.get("http://localhost:3000/doobie")
-      .end(function (e, res) {
-        (e === null).should.equal(true);
-        var response = res.body;
-        response = response[0];
-        response.should.have.property("name", "doobie");
-        done();
-      });
-  });
+            done();
+        });
 });
